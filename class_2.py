3d5f73f41443f5e3eb83f2e3663ce95022ab5907
Sona Gevorgyan <gevorgyans325@gmail.com>
2020-09-28 20:41:01 +0400

    New file_1 added

---
+++ class_2.py
@@ -0,0 +1,20 @@
+import math
+
+# my_name = "Sona"
+# surname = "Gevorgyan"
+# age = 20
+# phone_num = 33333333333
+#print("My full name is", my_name, surname,": I'm", age,"and my number", phone_num)
+
+# is_float = 3.7
+# is_bool = True
+# print(type(my_name), type(age), type(is_float), type(is_bool), sep='\n')
+
+# a = 7
+# b = 3
+# c = 4
+# d = (b**2+4*a*c)**0.5
+# print(d)
+
+print(math.pi)
+
