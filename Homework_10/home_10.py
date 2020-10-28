e32078848d3ed815ee7b61249ecfe23bafc2e09b
Sona Gevorgyan <gevorgyans325@gmail.com>
2020-10-29 02:17:43 +0400

    Homework_10

---
+++ Homework_10/home_10.py
@@ -0,0 +1,61 @@
+import json
+import threading
+import requests
+
+
+def function(l):
+
+
+	for i in l:
+
+		p = i["name"]
+		print(p)
+
+		with open(f"{p}.jpeg","wb") as f:
+			f.write(requests.get(i["url"]).content)
+
+
+	
+
+
+
+
+if __name__ == "__main__":
+
+	with open("image_url.json","r") as f:
+		dict_ = json.load(f)
+
+	
+	thread_list = []
+	a = []
+	j = 0
+	
+	for i in range(5):
+		a.append(dict_["items"][j])
+		a.append(dict_["items"][j+1])
+	
+		x = threading.Thread(target=function, args = (a, ))
+		thread_list.append(x)
+		x.start()
+	
+		a.clear()
+		j += 2
+
+
+	for thread in thread_list:
+		thread.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
