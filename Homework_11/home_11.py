468da540034d5868f0c16f70d7a6c8692f17e3ed
Sona Gevorgyan <gevorgyans325@gmail.com>
2020-11-03 19:07:25 +0400

    Homework_11_changed

--- Homework_11/home_11.py
+++ Homework_11/home_11.py
@@ -1,33 +1,35 @@
 import logging
 from random import randint
 
+logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.DEBUG)
 
 for i in range(50):
 	num = randint(0,50)
-	print(num)
 
 	if num < 10:
-		logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.DEBUG)
+		logging.debug(f"This is the debug level. We generate a random number and that number is {num}.")
+		logging.info(f"This is the info level, the number is {num}.")
+		logging.warning("This is the warning level, because the generate number is less than 30")
+		logging.error('This is the error level, because the generating number is less than 40.')
+		logging.critical("This is the critical level, because  generating number less 50.")
 	
 	elif num < 20:
-		logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.INFO)
+		logging.info(f"This is the info level, the number is {num}.")
+		logging.warning("This is the warning level, because the generate number is less than 30")
+		logging.error('This is the error level, because the generating number is less than 40.')
+		logging.critical("This is the critical level, because  generating number less 50.")
 
 	elif num < 30:
-		logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.WARNING)
+		logging.warning("This is the warning level, because the generate number is less than 30")
+		logging.error('This is the error level, because the generating number is less than 40.')
+		logging.critical("This is the critical level, because  generating number less 50.")
 
 	elif num < 40:
-		logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.ERROR)
+		logging.error('This is the error level, because the generating number is less than 40.')
+		logging.critical("This is the critical level, because  generating number less 50.")
 		
 	else:
-		logging.basicConfig(filename = "T.log", format = "%(levelname)s:%(asctime)s:%(message)s", level = logging.CRITICAL)
-
-
-
-	logging.debug(f"This is the debug level. We generate a random number and that number is {num}.")
-	logging.info(f"This is the info level, the number is {num}.")
-	logging.warning("This is the warning level, because the generate number is less than 30")
-	logging.error('This is the error level, because the generating number is less than 40.')
-	logging.critical("This is the critical level, because  generating number less 50.")
+		logging.critical("This is the critical level, because  generating number less 50.")
 
 
 
